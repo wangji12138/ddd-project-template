f5ddb10bc6e91009e94d2f24111d1cce714aa90d
wangji <13136143315@100fen.cn>
2020-11-30 15:29:30 +0800

    1.首次构建ddd模板项目 2.增加部分基础配置:mysql/es/redis等 3.增加部分基础依赖

---
+++ src/test/java/com/xxxxxxxxx/ddd/DddTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xxxxxxxxx.ddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
